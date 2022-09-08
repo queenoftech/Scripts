a49d7da3e2180e226d9ef400c45627fa4acefda8
ec2-user <natinda2006@gmail.com>
2022-09-08 17:41:04 +0000

    first commit

---
+++ ForLoop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#This is a for loop script
+#review,modify and execute accordingly
+echo "for loop script starts"
+for (( a=50;a<100;a++ ))
+do
+echo $a
+done
+echo "for loop ends"
+whoami
+date
